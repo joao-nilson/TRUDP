691afd0b21ab19805b2bd8a3aebfd1aab73182c8
joao-nilson <joao.nilson@estudante.ufjf.br>
2026-01-23 10:21:51 -0300

    add checksum calculation and header

--- packet.py
+++ packet.py
@@ -1,5 +1,6 @@
 import struct
-from daraclasses import dataclass
+import time
+from dataclasses import dataclass
 from enum import IntEnum
 
 # 0-4 ao invéz de flags binárias
@@ -10,25 +11,50 @@ class PacketType(IntEnum):
     SYN = 2
     SYN_ACK = 3
     FIN = 4
+    FIN_ACK = 5
+    RST = 6
 
 @dataclass
 class TRUPacket:
     seq_num: int = 0
     ack_num: int = 0
     packet_type: int = 0
+    window: int = 1024
+    checksum: int = 0
+    timestamp: float = 0.0
     data: bytes = b''
 
+    HEADER_SIZE = 25    #seq(4) + ack(4) + type(1) + window(2) + checksum(4) + timestamp(8)
+
     def serialize(self) -> bytes:
-        return struct.pack('!IIB', self.seq_num, self.ack_num, self.packet_type) + self.data
+        header = struct.pack('!IIBHIQ',
+                        self.seq_num,
+                        self.ack_num,
+                        self.packet_type,
+                        self.window,
+                        self.checksum,
+                        int(self.timestamp * 1000000))
+        return header + self.data
         
-
+    @classmethod
     def deserialize(cls, data: bytes) -> 'TRUPacket':
-        header = data[:9]
-        seq_num, ack_num, packet_type = struct.unpack('!IIB', header)
-        packet_data = data[:9] if len(data) > 9 else b''
+        if len(data) < cls.HEADER_SIZE:
+            raise ValueError("Pacote pequeno demais")
 
-        return cls(seq_num = seq_num,
-                   ack_num = ack_num,
-                   packet_type = packet_type,
-                   data = packet_data
+        header = data[:cls.HEADER_SIZE]
+        seq_num, ack_num, packet_type, window, checksum, timestamp = struct.unpack('!IIBHIQ', header[:24])
+        packet_data = data[cls.HEADER_SIZE:] if len(data) > cls.HEADER_SIZE else b''
+
+        return cls(
+            seq_num = seq_num,
+            ack_num = ack_num,
+            packet_type = packet_type,
+            window = window,
+            checksum = checksum,
+            timestamp = timestamp / 1000000.0
+            data = packet_data
         )
+    
+    def calculate_checksum(self) -> int:
+        data = self.serialize()
+        return sum(data) & 0xFFFFFFFF
